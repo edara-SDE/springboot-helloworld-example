d6c676205de0740c2aa2e9d2652372251d9366e3
edara-SDE <edararamakrishnaprasad140601@gmail.com>
2022-04-19 11:16:47 +0530

    first commit

---
+++ src/test/java/com/javatpoint/springboothelloworldexample/SpringBootHelloworldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.springboothelloworldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
